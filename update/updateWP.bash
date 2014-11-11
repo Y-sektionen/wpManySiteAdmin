0e3ae23a74ecd8b4f2fd8d92deb1049b7edbb8ea
CYD Admin <cydadmin@wp.cyd.liu.se>
2014-11-11 09:55:02 +0100

    Update works but needs polish in order to look good and only make minor updates.

---
+++ update/updateWP.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [[ $# != 0 || $S1 == "-h" || $S1 == "--help" ]]
+then
+        echo "Usage: "
+        echo "  $0"
+        exit 1
+fi
+
+# cd to script dir
+dir="$(dirname $0)"
+cd $dir
+
+# Read config file with paths to WP-installs and usernames
+config=$dir/updateWP.conf
+source $config
+
+for userName in $userNames
+do
+	echo "Updating site for user $userName..."
+	su - $userName -c "cd $basePath/$userName
+wp core update
+wp core update-db"
+	echo ""
+done
+
+
