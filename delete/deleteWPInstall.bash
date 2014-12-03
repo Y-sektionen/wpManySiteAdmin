87131a18104436594ffe353b9c9fda7d1546fad9
Rovanion Luckey <rovanion.luckey@gmail.com>
2014-12-03 16:09:13 +0100

    The delete script now sources the conf file.

--- delete/deleteWPInstall.bash
+++ delete/deleteWPInstall.bash
@@ -7,19 +7,22 @@ then
 	exit 1
 fi
 
+source "$(dirname $0)"/../conf
+
 userName=$1
 FQDN=$2
 
 # Remove users and DBs
-gpasswd -d www-data $userName
+gpasswd -d $userName www-data
 userdel $userName
+echo "Please enter root password for MYSQL: "
 mysql --user=root -p -e "drop database $userName;
 drop user '$userName'@'localhost';"
 
 # Remove configs
 rm /etc/nginx/sites-enabled/$FQDN
 rm /etc/nginx/sites-available/$FQDN
-rm /etc/php5/fpm/pool.d/"$FQDN".conf
+rm /etc/php5/fpm/pool.d/$FQDN.conf
 
 # Remove WP-install
-rm -r /srv/$userName
+rm -r $basePath/$userName
