569d7524ce078ba54f6b354c05e65f97f0eb89cb
Rovanion Luckey <rovanion.luckey@gmail.com>
2015-04-24 11:44:30 +0200

    Scanner that works, scanning all pages hosted by nginx on local machine

--- scan/scan.bash
+++ scan/scan.bash
@@ -1,21 +1,23 @@
 #!/bin/bash
-cd "$(dirname $0)"
-# Read config file with paths to WP-installs and usernames
-source ../conf
 
 # Update the git submodule(s) containing wpscan (and potentially other/future projects) so that we always run the latest version
+cd "$(dirname $0)/.."
 git submodule foreach git pull origin master
-# Set wpscan script
-wpscan=./wpscan/wpscan.rb
 
-sites = $(ls /etc/nginx/sites-enabled)
+# wpscan requires it's dependencies to be in PATH, easiest to just cd to folder
+cd "scan/wpscan"
+# Set wpscan script and sites to scan
+wpscan=./wpscan.rb
+sites=$(ls /etc/nginx/sites-enabled)
+# Update the database with vulnerabilities.
+$wpscan --update
 
-for site in sites
+for site in $sites
 do
+	echo ""
 	echo "------------"
 	echo "Scanning site $site for vulnerabilities using wpscan..."
-	wpscan -u $site --follow-redirection -e u,vt,vp
+	$wpscan -u $site --follow-redirection -e u,vt,vp
 	echo "------------"
-	echo ""
 done
 
