2bdb4f901eca640dca79d5a9454a52c4ca8a5c3a
hansfilipelo <hansfilipelo@gmail.com>
2016-02-01 09:59:50 +0100

    Fail safe if expLimit is not set

--- update/renewCerts.bash
+++ update/renewCerts.bash
@@ -8,12 +8,18 @@ source ../conf
 letsencFolder=/etc/letsencrypt
 dateNow=$(date -d "now" +%s)
 
+if [ -z $expLimit ]
+then
+	echo "Expiry date not set in conf. See conf.example for example."
+	exit 1
+fi
+
 
 # Update all certs with cli.ini file in folder for Let's Encrypt
 for configFile in $(ls $letsencFolder/*.cli.ini); do
 	
 	primaryDomain=`grep "^\s*domains" $configFile | sed "s/^\s*domains\s*=\s*//" | sed 's/(\s*)\|,.*$//'`
-	certFile=/etc/letsencrypt/live/$primaryDomain/fullchain.pem
+	certFile=$letsencFolder/live/$primaryDomain/fullchain.pem
 	expDate=$(date -d "`openssl x509 -in $certFile -text -noout | grep "Not After" | cut -c 25-`" +%s)
 	expDays=$(echo \( $expDate - $dateNow \) / 86400 |bc)
 	
