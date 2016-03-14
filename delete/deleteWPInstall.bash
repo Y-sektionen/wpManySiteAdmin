f3ef6ed6354915150802758a0ccab5b500425fdf
Martin Estgren <sci10n@users.noreply.github.com>
2016-03-14 12:46:21 +0100

    Escaped user defined variables
    There is no escape!

--- delete/deleteWPInstall.bash
+++ delete/deleteWPInstall.bash
@@ -12,35 +12,34 @@ cd "$(dirname $0)"
 source ../conf
 
 
-userName=$1
-FQDN=$2
+userName="$1"
+FQDN="$2"
 letsencryptFolder=/etc/letsencrypt
 
 # Remove users and DBs
-userdel $userName
-groupdel $userName
+userdel "$userName"
+groupdel "$userName"
 echo "Please enter root password for MYSQL: "
 mysql --user=root -p -e "drop database $userName;
 drop user '$userName'@'localhost';"
 
 # Remove configs
-rm -f /etc/nginx/sites-enabled/$FQDN
-rm -f /etc/nginx/sites-available/$FQDN
-rm -f /etc/php5/fpm/pool.d/$FQDN.conf
+rm -f /etc/nginx/sites-enabled/"$FQDN"
+rm -f /etc/nginx/sites-available/"$FQDN"
+rm -f /etc/php5/fpm/pool.d/"$FQDN".conf
 
 # Revoke certificate. Will try to revoke all certs. 
-for certificate in $(ls $letsencryptFolder/archive/$FQDN | grep "cert")
-do
+for certificate in $(ls $letsencryptFolder/archive/$FQDN | grep "cert"); do
 	echo "Revoking cert $certificate"
-	../letsencrypt/letsencrypt-auto revoke --cert-path $letsencryptFolder/archive/$FQDN/$certificate
+	../letsencrypt/letsencrypt-auto revoke --cert-path "$letsencryptFolder"/archive/"$FQDN"/"$certificate"
 	echo ""
 done
 # Remove config for domain
-rm -f $letsencryptFolder/$FQDN.cli.ini
+rm -f "$letsencryptFolder"/"$FQDN".cli.ini
 
 # Remove WP-install
-rm -rf $basePath/$userName
-rm -rf /home/$userName/.wp-cli
+rm -rf "$basePath"/"$userName"
+rm -rf /home/"$userName"/.wp-cli
 
 systemctl reload nginx php5-fpm
 
